60ac13978350d9054db60a093f67d2fa8234b7e9
Zhavoronkov Nikita <n0962783666@gmail.com>
2020-10-09 11:10:41 +0300

    Do some work

--- source.py
+++ source.py
@@ -1,10 +1,11 @@
 # 2020, all rights reserved
 
-from collections import namedtuple as tuple
-
 #global variables
 routes = dict()
 stations = list()
+questions = dict()
+constants = dict()
+variables = list()
 
 def read_routes(filename):
     ''' key : tram_id
@@ -19,6 +20,53 @@ def read_all_stations(filename):
     with open(filename, encoding='utf-8', mode='r') as f:
         global stations
         stations = f.read().splitlines()
+
+def read_questions(filename):
+    def add_to_dict(dictionary,value,key):
+        space = value.find(' ')
+        firstWord = value[:space+1]
+        if space == -1:
+            dictionary[value] = key
+        elif firstWord in dictionary.keys():
+            add_to_dict(dictionary[firstWord],value[space+1:],key)
+        else:
+            dictionary[firstWord] = dict()
+            add_to_dict(dictionary[firstWord],value[space+1:],key)
+
+    def remove_solo(key,dictionary):
+        if '+' in dictionary.keys() and '-' in dictionary.keys():
+            return (key,dictionary);
+        res = dict()
+        if len(dictionary.keys()) == 1:
+            if list(dictionary.keys())[0].startswith('$'):
+                (tk,dc) = remove_solo('',list(dictionary.values())[0])
+                return  (key,{list(dictionary.keys())[0]:{tk:dc}})
+            return remove_solo(key.strip() + " " + list(dictionary.keys())[0],list(dictionary.values())[0])
+        else:
+            for k in dictionary.keys():
+                (tk,dc) = remove_solo(k,dictionary[k])
+                res[tk] = dc
+            return (key,res)
+
+    temp = dict()
+    with open(filename,'r',encoding = 'UTF-8') as questionsFile:
+        for line in questionsFile.readlines():
+            qa = line.split('->')
+            question = qa[0].strip();
+            answers = qa[1].split('&')
+            answers = {"+" : answers[0],"-" : answers[1], 'func' : howto_get_to_n}
+            add_to_dict(temp,question,answers)
+    global questions
+    questions = remove_solo('',temp)[1]
+
+def do_all_preparations():
+    read_routes("all_trams.txt")
+    read_all_stations("all_stops.txt")
+    read_questions("question-answer.txt")
+    constants['$tram_id'] = routes.keys()
+    constants['$station_id'] = stations
+
+
 
 #1 query
 def count_of_stations_for_tram(tram_id):
@@ -54,120 +102,122 @@ def get_to_university(station_):
 def is_possible_to_get(station_id, tram_id):
     return False
 
-def is_before(found_list, start_station, end_station):
-    start = found_list.index(start_station)
-    end = found_list.index(end_station)
-    return start < end
+def get_route_from_list(direction,start_station,end_station):
+    start = listLower(direction).index(start_station.lower())
+    end = listLower(direction).index(end_station.lower())
+    reverse = False
+    if start > end:
+        temp = start
+        start = end
+        end = temp
+        reverse = True
+    res = direction[start:end+1]
+    if reverse:
+        res.reverse()
+    return res
 
-def get_route_from_list(found_list,start_station,end_station):
-    start = found_list.index(start_station)
-    end = found_list.index(end_station)
-    return found_list[start:end+1]
+def listLower(lst):
+    return [x.lower() for x in lst]
 
 def howto_get_to_n(start_station,end_station):
-    found_list = list()
     # with no redirections
     for k in routes:
-        if start_station in routes[k][0] and end_station in routes[k][0] and is_before(routes[k][0], start_station, end_station):
-            found_list = routes[k][0]
-            print("no redirections by " + k)
-            return get_route_from_list(found_list, start_station, end_station)
-        elif start_station in routes[k][1] and end_station in routes[k][1] and is_before(routes[k][1], start_station, end_station): # in backward
-            found_list = routes[k][1]
-            print("no redirections by " + k)
-            return get_route_from_list(found_list, start_station, end_station)
-            
+        for direction in routes[k]:
+            if start_station.lower() in listLower(direction) and end_station.lower() in listLower(direction):
+                return (k,get_route_from_list(direction, start_station, end_station))
+    # with one redirection 
     for t1 in routes:
-        print("#" + t1)
+        direction1 = list()
         known_station = object()
         unknown_station = object()
-        state = True
-        if start_station in routes[t1][0] and state:
-            known_station = start_station
-            unknown_station = end_station
-            found_list = routes[t1][0]
-            state = False
-        elif start_station in routes[t1][1] and state:
-            known_station = start_station
-            unknown_station = end_station
-            found_list = routes[t1][1]
-            state = False
-        elif end_station in routes[t1][0] and state:
-            known_station = end_station
-            unknown_station = start_station
-            found_list = routes[t1][0]
-            state = False
-        elif end_station in routes[t1][1] and state:
-            known_station = end_station
-            unknown_station = start_station
-            found_list = routes[t1][1]
-            state = False
-        for t2 in routes.keys():
+
+        for direction in routes[t1]:
+            if start_station.lower() in listLower(direction):
+                known_station = start_station.lower()
+                unknown_station = end_station.lower()
+                direction1 = direction
+                break
+            elif end_station.lower() in listLower(direction):
+                known_station = end_station.lower()
+                unknown_station = start_station.lower()
+                direction1 = direction
+                break
+
+        if len(direction1) == 0:
+            continue
+
+        for t2 in routes:
             if t1 == t2:
                 continue
-            if unknown_station in routes[t2][0]:
-                common_stations = list(set(found_list).intersection(set(routes[t2][0])))
-                if len(common_stations) != 0:
-                    common_station = common_stations[0]
-                    print("#" + t2)
-                    return get_route_from_list(found_list, start_station, common_station) + get_route_from_list(routes[t2][0], common_station, unknown_station)
-            elif unknown_station in routes[t2][1]:
-                common_stations = list(set(found_list).intersection(routes[t2][1]))
-                if len(common_stations) != 0:
-                    common_station = common_stations[len(common_stations)-1]
-                    print("#" + t2)
-                    return get_route_from_list(found_list, known_station, common_station) + get_route_from_list(routes[t2][1], common_station, unknown_station) 
+            for direction2 in routes[t2]:
+                if unknown_station in direction2:
+                    common_stations = list(set(direction1) & set(direction2))
+                    if len(common_stations) != 0:
+                        common_station = common_stations[0]
+                        return [(t1,get_route_from_list(direction1, known_station, common_station)), (t2,get_route_from_list(direction2, common_station, unknown_station))]
+    # more than one redirection
     return list()
 
-            
-def write_to_file_dialog(content):
-    ''' @param content : list of strings
-        introduce console dialog for writrinf to file
-        @return void '''
+def navigate_user(dictionary,string,prev):
+    found = False
+    if list(dictionary.keys())[0].startswith('$'):
+        key = list(dictionary.keys())[0].strip()
+        if string.strip() == '':
+            if '?' in key:
+                print(prev + key.replace('$',''))
+            else:
+                print(prev + key.replace('$','') + ' ...')
+            print('Можливі значення '+key.replace('$','').replace('?','') + ':')
+            print('\t'.join(constants[key.replace('?','')]))
+            return (False,{})
+        global variables
+        toFind = string.replace('?','')
+        if string.strip().startswith('"'):
+            toFind = string[string.find('"'):]
+            toFind = toFind[:toFind[1:].find('"')+2]
+        else:
+            toFind = string.strip().split(' ')[0]
+        if toFind.lower().strip().replace('"','') in [x.lower().strip() for x in constants[key.replace('?','')]]:
+            variables.append(toFind.lower().replace('"',''))
+            if '?' in key:
+                return (True,list(dictionary.values())[0])
+            return navigate_user(list(dictionary.values())[0],string[string.find(toFind)+len(toFind):],prev + toFind.strip())
+        else:
+            print('Не вдалося знайти {} {}'.format(key.replace('$','').replace('?',''),toFind))
+            print('Можливі значення '+key.replace('$','').replace('?','') + ':')
+            print('\t'.join(constants[key.replace('?','')]))
+            return (False,{})
 
-    print("Do u want to save this output to file ?")
-    ans = input("[YES or NO] : ")
-    if ans.lower() == "yes" or ans.lower() in "yes":
-        print("Enter a filename to save data : [with no extension]")
-        filename = input(">>> ")
-        write_to_file(filename, content)
+    if string != '':
+        for k in dictionary.keys():
+            if k.lower().startswith(string.lower()) or string.lower().startswith(k.lower()):
+                found = True
+                if k.endswith('?'):
+                    return (True,dictionary[k])
+                else:
+                    return navigate_user(dictionary[k],string[len(k):],prev + k)
 
-def write_to_file(filename, content):
-    ''' @param filename : name of file to write [with no extenstion]
-        @param context : data to be written 
-        Create new or rewrite if exists file with content in .txt format
-        @return void '''
+    if not found:
+        for word in string.split(' '):
+            found_two = False
+            for k in dictionary.keys():
+                if word.lower() in k.lower():
+                    found_two = True
+            if not found_two:
+                print("Unknown word : " + word)
+        for k in dictionary.keys():
+            if k.endswith('?'):
+                print(prev + k)
+            else:
+                print(prev + k + '...')
+    return (False,{})
 
-    with open(filename + ".txt", encoding='utf-8', mode='w') as f:
-        for i in content:
-            f.write(i)
-            f.write("\n")
-        f.close()
-
-
-continue_execution = True
-
-while continue_execution:
-    print('# 1 : Exit')
-    try:
-        state = int(input("Maka a choice pls >>> "))
-    except ValueError:
-        print("Choose from a list!")
-        continue
-    if state == 1:
-        print("Thanks for usage! Really appreciate that!")
-        continue_execution = False
-    if state == 2:
-        print("All stops file")
-        filename = input(">>> ")
-        read_all_stations(filename)
-        for i in stations:
-            print(i)
-    if state == 3:
-        print("All routes file")
-        filename = input(">>> ")
-        read_routes("all_trams.txt")
-        print(routes)
-    if state == 4:
-        print("1")
-        print(howto_get_to_n("станція Підзамче", "вул. Русових"))
+do_all_preparations()
+print('Введіть питання, початок питання чи просто натисніть "Enter" : ')
+while(True):
+    variables = list()
+    (questionFull,handler) = navigate_user(questions,input(),'')
+    if questionFull:
+        handler = handler['']
+        print(variables)
+        print(handler['func'](*variables))
--- ui.py
+++ ui.py
@@ -1,124 +1,23 @@
-input_helper = dict()
-q_a = dict()
-constants = {"$tram_id":['0','1'],'$station_id':['aaaba','b','c']}
-variables = list()
-def add_to_dict(dictionary,value,key):
-	space = value.find(' ')
-	firstWord = value[:space+1]
-	if space == -1:
-		dictionary[value] = key
-	elif firstWord in dictionary.keys():
-		add_to_dict(dictionary[firstWord],value[space+1:],key)
-	else:
-		dictionary[firstWord] = dict()
-		add_to_dict(dictionary[firstWord],value[space+1:],key)
+def write_to_file_dialog(content):
+    ''' @param content : list of strings
+        introduce console dialog for writrinf to file
+        @return void '''
 
-with open('question-answer.txt','r',encoding = 'UTF-8') as questions:
-	for line in questions.readlines():
-		qa = line.split('->')
-		question = qa[0].strip();
-		answers = qa[1].split('&')
-		answers = {"+" : answers[0],"-" : answers[1]}
-		add_to_dict(input_helper,question,answers)
+    print("Do u want to save this output to file ?")
+    ans = input("[YES or NO] : ")
+    if ans.lower() == "yes" or ans.lower() in "yes":
+        print("Enter a filename to save data : [with no extension]")
+        filename = input(">>> ")
+        write_to_file(filename, content)
 
-def remove_solo(key,dictionary):
-	if '+' in dictionary.keys() and '-' in dictionary.keys():
-		return (key,dictionary);
-	res = dict()
-	if len(dictionary.keys()) == 1:
-		if list(dictionary.keys())[0].startswith('$'):
-			(tk,dc) = remove_solo('',list(dictionary.values())[0])
-			return  (key,{list(dictionary.keys())[0]:{tk:dc}})
-		return remove_solo(key.strip() + " " + list(dictionary.keys())[0],list(dictionary.values())[0])
-	else:
-		for k in dictionary.keys():
-			(tk,dc) = remove_solo(k,dictionary[k])
-			res[tk] = dc
-		return (key,res)
+def write_to_file(filename, content):
+    ''' @param filename : name of file to write [with no extenstion]
+        @param context : data to be written 
+        Create new or rewrite if exists file with content in .txt format
+        @return void '''
 
-def print_dict(dictionary,cnt):
-	for k in dictionary.keys():
-		print('    '*cnt + k)
-		if type(dictionary[k]) == dict:
-			print_dict(dictionary[k],cnt+1)
-		else:
-			print('    '*cnt + dictionary[k])
-
-res = remove_solo('',input_helper)[1]
-#print_dict(res,0)
-
-def myIn(string,findIn):
-	smaller = string if len(string) < len(findIn) else findIn
-	bigger = string if smaller == findIn else findIn
-	if smaller in bigger:
-		return True
-	count = 0
-	for i in range(len(smaller)):
-		if smaller[i] == bigger[i]:
-			count += 1
-	return (count/(float)(len(smaller)))>=0.5
-
-def myInSet(string, arr):
-	if string in arr:
-		return (True,string)
-	for el in arr:
-		if myIn(string,el):
-			return (True,el)
-	return (False,'')
-
-def navigate_user(dictionary,string,prev):
-	found = False
-	if list(dictionary.keys())[0].startswith('$'):
-		key = list(dictionary.keys())[0].strip()
-		if string.strip() == '':
-			if '?' in key:
-				print(prev + key.replace('$',''))
-			else:
-				print(prev + key.replace('$','') + ' ...')
-			print('Можливі значення '+key.replace('$','').replace('?','') + ':')
-			print('\t'.join(constants[key.replace('?','')]))
-			return (False,{})
-		global variables
-		toFind = string.replace('?','')
-		if string.find(' ')!=-1:
-			toFind = string[:string.find(' ')]
-		(inSet, properName) = myInSet(toFind,constants[key.replace('?','')])
-		if inSet:
-			variables.append(properName)
-			if '?' in key:
-				return (True,list(dictionary.values())[0])
-			if string.find(' ') == -1 or string.find(' ') == len(string):
-				return navigate_user(list(dictionary.values())[0],'',prev + properName)
-			else:
-				return navigate_user(list(dictionary.values())[0],string[string.find(' ')+1:],prev + properName+' ')
-		else:
-			print('Не вдалося знайти {} {}'.format(key.replace('$','').replace('?',''),toFind))
-			print('Можливі значення '+key.replace('$','').replace('?','') + ':')
-			print('\t'.join(constants[key.replace('?','')]))
-			return (False,{})
-
-	if string != '':
-		for k in dictionary.keys():
-			if myIn(k.strip(),string.strip()):
-				found = True
-				if k.endswith('?'):
-					return (True,dictionary[k])
-				else:
-					return navigate_user(dictionary[k],string[len(k):],prev + k)
-
-	if not found:
-		for word in string.split(' '):
-			found_two = False
-			for k in dictionary.keys():
-				if word in k:
-					found_two = True
-			if not found_two:
-				print("Unknown word : " + word)
-		for k in dictionary.keys():
-			if k.endswith('?'):
-				print(prev + k)
-			else:
-				print(prev + k + '...')
-	return (False,{})
-
-print(navigate_user(res,'Скільки зупинок між','')[0])
+    with open(filename + ".txt", encoding='utf-8', mode='w') as f:
+        for i in content:
+            f.write(i)
+            f.write("\n")
+        f.close()
